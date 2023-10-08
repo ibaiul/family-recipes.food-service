8175591e3d01519074ac5ace5948598eeed8b236
Ibai Usubiaga <8058261+ibaiul@users.noreply.github.com>
2023-10-09 01:40:02 +0200

    Adapt local docker dev environment

--- run-latest-write.sh
+++ run-latest-write.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-docker-compose -f docker/docker-compose-local.yml stop write-model
-docker rm food-service-write-model -f
+docker-compose -f docker/docker-compose-local.yml stop write-model1 write-model2
+docker rm food-service-write-model1 food-service-write-model2 -f
 ./gradlew clean food-service-write-model:bootJar
 docker build -t ibaiul/family-recipes:food-service-write-model-local --build-arg MODULE_NAME=write-model -f Dockerfile .
 docker-compose -f docker/docker-compose-local.yml up -d
-docker logs food-service-write-model -f
+docker logs food-service-write-model1 -f
